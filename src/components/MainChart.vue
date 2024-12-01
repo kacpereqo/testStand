092c42cf845361a8bb410f246f467430e66866bc
kacpereqo <remzak.k@gmail.com>
2024-12-01 18:27:45 +0100

    feat: add chart button

--- src/components/MainChart.vue
+++ src/components/MainChart.vue
@@ -1,11 +1,23 @@
 <template>
   <fieldset class="wrapper">
     <legend>Chart</legend>
-    <div id="chart"></div>
+    <div class="add-chart">
+      <div class="tooltip">
+        <ul>
+          <li @click="addChart" class="add-chart"><span>+</span></li>
+        </ul>
+      </div>
+    </div>
   </fieldset>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const charts = ref([])
+
+function addChart() {}
+</script>
 
 <style scoped>
 .wrapper {
@@ -14,6 +26,34 @@
   flex-direction: column;
   border-radius: 4px;
   border: 2px solid var(--color-border);
+}
+
+.tooltip {
+  display: flex;
+  justify-content: end;
+}
+
+.tooltip ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+}
+
+.tooltip li {
+  cursor: pointer;
+  border-radius: 8px;
+  background-color: var(--color-accent);
+  color: var(--color-text);
+  height: 2rem;
+  min-width: 2rem;
+  border: 1px solid var(--color-border);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.tooltip li:hover {
+  background-color: var(--color-hover);
 }
 
 legend {
